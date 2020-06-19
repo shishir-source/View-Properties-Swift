a3e7c05883422a3437b693a0961e3f49094354bf
Shishir <shishir323@gmail.com>
2020-06-19 21:06:48 +0600

    Initial Commit

---
+++ ViewProperties/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewProperties
+//
+//  Created by Shishir Ahmed on 19/6/20.
+//  Copyright © 2020 Shishir Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
